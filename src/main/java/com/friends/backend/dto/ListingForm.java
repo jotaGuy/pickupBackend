52d0c8c678530437dc7881e3b780cc8d319b5df8
jancyj <jotabbusiness@gmail.com>
2024-06-09 01:28:19 -0500

    Removed used imports

--- src/main/java/com/friends/backend/dto/ListingForm.java
+++ src/main/java/com/friends/backend/dto/ListingForm.java
@@ -2,11 +2,8 @@ package com.friends.backend.dto;
 
 
 import com.friends.backend.entity.Location;
-import com.friends.backend.entity.Person;
-import jakarta.persistence.*;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
-import jakarta.validation.constraints.Size;
 import lombok.Getter;
 import lombok.Setter;
 import org.springframework.validation.annotation.Validated;
