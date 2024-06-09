e01807f2e99c129c4a9b4784c08fbdbaa2ad095a
jancyj <jotabbusiness@gmail.com>
2024-06-09 01:30:11 -0500

    Removed used imports

--- src/main/java/com/friends/backend/controller/HomeController.java
+++ src/main/java/com/friends/backend/controller/HomeController.java
@@ -4,7 +4,6 @@ import com.friends.backend.dto.UserForm;
 import lombok.AllArgsConstructor;
 import lombok.NoArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
